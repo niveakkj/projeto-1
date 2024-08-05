890fcfb914f8b46e681d2646474fbfe52a132ca6
niveakkj <00001108110654sp@al.educacao.sp.gov.br>
2024-08-05 17:02:49 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(1000, 800);
+  background("black");
+}
+
+function draw() {
+  
+ 
+  fill("rgb(156,43,53)");
+  stroke("white");
+  if(mouseIsPressed)
+   circle(mouseX,mouseY,50);
+  
+  
+  
+}
